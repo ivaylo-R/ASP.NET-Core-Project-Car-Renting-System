0b48351702e5a88a0e788776467637e969c9952d
Ivaylo Rusinov <ivaylor2@gmail.com>
2021-07-14 16:10:52 +0300

    Create CarsController

--- CarRentingSystem/Controllers/CarsController.cs
+++ CarRentingSystem/Controllers/CarsController.cs
@@ -1,11 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CarRentingSystem.Data;
+using CarRentingSystem.Data.Models;
+using CarRentingSystem.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarRentingSystem.Controllers
 {
-    public class CarsController
+    public class CarsController : Controller
     {
+        private CarRentingDbContext data;
+
+        public CarsController(CarRentingDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
+        {
+            if (data.Cars.Any(c=>c.Id !=car.Id))
+            {
+                return View("Error");
+            }
+
+            var carData = new Car
+            {
+                Id = car.Id,
+                Brand = car.Brand,
+                Model = car.Model,
+                Year = car.Year,
+                ImageUrl = car.ImageUrl,
+                CategoryId = car.CategoryId,
+                Description = car.Description,
+            };
+
+            data.Cars.Add(carData);
+
+            data.SaveChanges();
+
+            return Redirect("Home");
+        }
     }
 }
---
+++ CarRentingSystem/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentingSystem.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        public int Id { get; init; }
+
+        public string Brand { get; init; }
+
+        public string Model { get; init; }
+
+        public string Description { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Year { get; init; }
+
+        public int CategoryId { get; init; }
+
+    }
+}
